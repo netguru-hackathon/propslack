4852a1c0cbc566fa3838db89faa1d41308ecfa2e
wojzag <wojciech.zagorski@netguru.pl>
2016-06-24 15:26:06 +0200

    something

--- main.go
+++ main.go
@@ -2,30 +2,42 @@
 package main
 
 import (
-  "log"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 
-  if port == "" {
-    log.Fatal("$PORT must be set")
-  }
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
-  router := gin.Default()
+	router := gin.Default()
+	router.POST("/props", func(c *gin.Context) {
+		user_id := c.PostForm("user_id")
+		user_name := c.PostForm("user_name")
 
-  router.POST("/props", func(c *gin.Context) {
-    user_id := c.PostForm("user_id")
-    user_name := c.PostForm("user_name")
-
-    c.JSON(200, gin.H{
-      "status":  "posted",
-      "user_id": user_id,
-      "nick":    user_name,
-    })
-  })
-  router.Run(":" + port)
+		c.JSON(200, gin.H{
+			"status":  "posted",
+			"user_id": user_id,
+			"nick":    user_name,
+		})
+		resp, err := http.Get("https://slack.com/api/users.info?user=" + user_id + "&token=xoxp-52288129089-52448067687-54001525427-8d1a069a69")
+		if err != nil {
+			// handle error
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			// handle error
+		}
+		fmt.Println(body)
+	})
+	router.Run(":" + port)
 }
