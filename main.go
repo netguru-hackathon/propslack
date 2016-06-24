a7f2012a4fc45c3db0ef7ca3534fdcc9b9a6719a
wojzag <wojciech.zagorski@netguru.pl>
2016-06-24 17:12:18 +0200

    add fuction for converting mention name to email, first name and last name

--- main.go
+++ main.go
@@ -12,21 +12,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type ProfileJson struct {
-	Email string `json:"email"`
+type Profile struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
 }
 type Member struct {
-	Profile ProfileJson `json:"profile"`
-	Id      string      `json:"id"`
-	Name    string      `json:"name"`
+	Profile Profile `json:"profile"`
+	Id      string  `json:"id"`
+	Name    string  `json:"name"`
 }
 type User struct {
-	Member []Member `json:"members"`
+	Members []Member `json:"members"`
 }
 
 func main() {
 	port := os.Getenv("PORT")
-	token := os.Getenv("TOKEN")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -37,21 +38,7 @@ func main() {
 		user_id := c.PostForm("user_id")
 		user_name := c.PostForm("user_name")
 
-		resp, err := http.Get("https://slack.com/api/users.list?user=" + user_id + "&token=" + token)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-
-		var dat User
-		if err := json.Unmarshal(body, &dat); err != nil {
-			panic(err)
-		}
-		fmt.Println(dat)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
+		fmt.Printf("%+v\n", convertNameToID("wojzag"))
 
 		c.JSON(200, gin.H{
 			"status":  "posted",
@@ -60,4 +47,29 @@ func main() {
 		})
 	})
 	router.Run(":" + port)
+}
+
+func convertNameToID(s string) (p Profile) {
+	token := os.Getenv("TOKEN")
+	resp, err := http.Get("https://slack.com/api/users.list?token=" + token)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var user User
+	if err := json.Unmarshal(body, &user); err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(user.Members); i++ {
+		if user.Members[i].Name == s {
+			p.Email = user.Members[i].Profile.Email
+			p.FirstName = user.Members[i].Profile.FirstName
+			p.LastName = user.Members[i].Profile.LastName
+			return
+		}
+	}
+	return
 }
