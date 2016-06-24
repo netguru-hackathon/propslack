5a9cc1446460fa42b7687dd3b5f09e3c5db526f7
Dawid Jaskot <dawid.jaskot@netguru.co>
2016-06-24 14:53:37 +0200

    Fix indentation

--- main.go
+++ main.go
@@ -2,30 +2,30 @@
 package main
 
 import (
-	"log"
-	"os"
+  "log"
+  "os"
 
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+  port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+  if port == "" {
+    log.Fatal("$PORT must be set")
+  }
 
-	router := gin.Default()
+  router := gin.Default()
 
-	router.POST("/props", func(c *gin.Context) {
-		user_id := c.PostForm("user_id")
-		user_name := c.PostForm("user_name")
+  router.POST("/props", func(c *gin.Context) {
+    user_id := c.PostForm("user_id")
+    user_name := c.PostForm("user_name")
 
-		c.JSON(200, gin.H{
-			"status":  "posted",
-			"user_id": user_id,
-			"nick":    user_name,
-		})
-	})
-	router.Run(":" + port)
+    c.JSON(200, gin.H{
+      "status":  "posted",
+      "user_id": user_id,
+      "nick":    user_name,
+    })
+  })
+  router.Run(":" + port)
 }
